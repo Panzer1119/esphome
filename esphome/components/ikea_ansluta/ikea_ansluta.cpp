ed50e17ba87de7359bde778194aa29d7824c9e2c
Paul Hagedorn <panzer1119@gmail.com>
2024-01-25 18:10:05 +0100

    feat(ikea_ansluta): create class and namespace ikea_ansluta ikea_ansluta

---
+++ esphome/components/ikea_ansluta/ikea_ansluta.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Paul on 2024-01-25.
+//
+
+#include "ikea_ansluta.h"
+
+namespace ikea_ansluta {}  // namespace ikea_ansluta
---
+++ esphome/components/ikea_ansluta/ikea_ansluta.h
@@ -0,0 +1,14 @@
+//
+// Created by Paul on 2024-01-25.
+//
+
+#ifndef ESPHOME_IKEA_ANSLUTA_H
+#define ESPHOME_IKEA_ANSLUTA_H
+
+namespace ikea_ansluta {
+
+class ikea_ansluta {};
+
+}  // namespace ikea_ansluta
+
+#endif  // ESPHOME_IKEA_ANSLUTA_H
